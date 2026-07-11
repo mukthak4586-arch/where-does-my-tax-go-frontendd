ad78202b3864fceb032d9b9ad49e536da97ca3a9
Your Name <your_email@gmail.com>
2026-07-11 18:09:52 +0530

    Connect Render backend

--- src/services/meshApi.js
+++ src/services/meshApi.js
@@ -1,17 +1,16 @@
 import axios from "axios";
 
+const API = "https://tax-ai-backend-i8h5.onrender.com/chat";
+
 export async function askMesh(question) {
   try {
-    const response = await axios.post(
-      "http://localhost:5000/chat",
-      {
-        question,
-      }
-    );
+    const response = await axios.post(API, {
+      question,
+    });
 
     return response.data.answer;
-  } catch (error) {
-    console.error(error);
-    return "Unable to connect to backend.";
+  } catch (err) {
+    console.error(err);
+    return "Unable to connect to AI server.";
   }
 }
